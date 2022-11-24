383d448d001a5a8e9d8a6c8a79e41e6de1c76e04
Anhwa <63187312+Anhwa8@users.noreply.github.com>
2022-11-24 18:31:03 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 384 ms, Memory: 20344 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception  {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+
+		for(int i=1; i<=N; i++){
+			for(int j=1; j <= N-i; j++){
+				System.out.print(" ");
+			}
+			for(int k=0; k<i; k++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+    }
+}
