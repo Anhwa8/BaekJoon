af56b0f9da9b181dd8a37a68055c9cf1bc2033f5
Anhwa <63187312+Anhwa8@users.noreply.github.com>
2022-11-24 18:18:51 +0900

    [Bronze V] Title: A+B - 7, Time: 252 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception  {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		int A, B;
+
+		for(int i=1; i<=T; i++){
+			A = in.nextInt();
+			B = in.nextInt();
+			System.out.println("Case #" + i + ": " + (A+B));
+		}
+    }
+}
